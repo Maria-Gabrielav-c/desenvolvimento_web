35ce5555a96785d53188e328fa6a511867fce7ff
Maria-Gabrielav-c <maria.chaves2@estudante.ifgoiano.edu.br>
2024-09-02 10:21:30 -0300

    aula01

---
+++ hello.js
@@ -0,0 +1,8 @@
+// node hello.js
+console.log('Olá do Servidor'); 
+
+/**
+ * node é a ferramenta que vai nos entregar a capacidade de interpretar código JavaScript, de maneira bem similar ao navegador - https://www.alura.com.br/artigos/node-js
+ * node js é um ambiente do servidor open surce - by Julio
+ * node js usa JavaScript no servidor
+ */
